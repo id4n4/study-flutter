dc29cc49227c2d4155c4804082fb5aa3f7a9d1ef
david sanchez <nagatiitoo@hotmail.com>
2024-08-27 09:08:46 -0500

    add theme config

---
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const colorList = [
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.purple,
+  Colors.orange,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  }) : assert(selectedColor >= 0 && selectedColor < colorList.length,
+            'Selected color index is out of range 0 - ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: colorList[selectedColor],
+        ),
+      );
+}
